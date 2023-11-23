6ee50e6bbc284ea321188b4a763c52bc2ea7f7a7
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-11-22 17:43:11 -0800

    devsql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,13 @@
--- Creates the database hbnb_dev_db with specified paramenters
+-- Creates the database hbnb_dev_db with specified parameters
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Creates user if doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
--- Sets password for user
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
--- Grants privileges to user on database
+
+-- Creates user if doesn't exist and sets password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grants privileges to user on database and performance_schema
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grants select privileges to user on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- Flush privileges
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
