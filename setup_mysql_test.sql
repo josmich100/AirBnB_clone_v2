0764c2cd2966d6667c5ae0740d600d078528dc7b
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-11-23 04:42:13 +0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,13 +1,13 @@
--- Creates the database hbnb_test_db with specified paramenters
+-- Creates the database hbnb_test_db with specified parameters
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- Creates user if doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
--- Sets password for user
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
--- Grants privileges to user on database
+
+-- Creates user if doesn't exist and sets password
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grants privileges to user on database and performance_schema
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
--- Grants select privileges to user on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
--- Flush privileges
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
