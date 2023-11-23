20a93c62243ecf97badd8f86c2a4a09c8a1525ba
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-11-23 04:19:07 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,18 +2,14 @@
 """Defines the DBStorage engine."""
 from os import getenv
 from models.base_model import Base
-from models.base_model import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-
 
 class DBStorage:
     """Represents a database storage engine.
@@ -28,35 +24,29 @@ class DBStorage:
 
     def __init__(self):
         """Initialize a new DBStorage instance."""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
+        # Use f-strings for better readability
+        self.__engine = create_engine(f"mysql+mysqldb://{getenv('HBNB_MYSQL_USER')}:{getenv('HBNB_MYSQL_PWD')}@{getenv('HBNB_MYSQL_HOST')}/{getenv('HBNB_MYSQL_DB')}",
                                       pool_pre_ping=True)
         if getenv("HBNB_ENV") == "test":
+            # Use declarative_base instead of Base.metadata.drop_all for better code organization
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
+        """Query on the current database session all objects of the given class.
 
         If cls is None, queries all types of objects.
 
-        Return:
+        Returns:
             Dict of queried classes in the format <class name>.<obj id> = obj.
         """
+        # Simplify the code using a dictionary comprehension
         if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
+            classes = [State, City, User, Place, Review, Amenity]
+            objs = [obj for cls in classes for obj in self.__session.query(cls).all()]
         else:
-            if type(cls) == str:
-                cls = eval(cls)
-            objs = self.__session.query(cls)
-        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+            # Use isinstance for checking the type
+            objs = self.__session.query(cls) if isinstance(cls, type) else self.__session.query(eval(cls))
+        return {f"{type(o).__name__}.{o.id}": o for o in objs}
 
     def new(self, obj):
         """Add obj to the current database session."""
@@ -68,14 +58,14 @@ class DBStorage:
 
     def delete(self, obj=None):
         """Delete obj from the current database session."""
-        if obj is not None:
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
         """Create all tables in the database and initialize a new session."""
+        # Use declarative_base instead of Base.metadata.create_all for better code organization
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
